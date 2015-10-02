0cc5e38ece5f1375be817716b1028d5f67e6f3ea
4current <rich.morrisey@gmail.com>
2015-10-02 18:04:29 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'jekyll_blog_morrisey'
+maintainer       'Richard A. Morrisey'
+maintainer_email 'rich.morrisey@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures jekyll_blog_morrisey'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'jekyll', '~> 0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: jekyll_blog_morrisey
+# Recipe:: default
+#
+# Copyright 2015, Richard A. Morrisey
+#
+# All rights reserved - Do Not Redistribute
+#
